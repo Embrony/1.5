ee586192b1f13383838535595304de39ac76984d
Embrony_Stone <cicada5601@mail.ru>
2024-12-01 20:57:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    width: 240,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
